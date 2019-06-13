38585c8b4442aabb2a349a61fff9768e64cfe9bb
Bailey Tincher <btincher99@gmail.com>
2019-06-13 04:28:24 -0500

    Finish cards

--- src/Components/Claim/Claim.jsx
+++ src/Components/Claim/Claim.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -23,7 +24,7 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    width: '100%',
     overflow: 'hidden',
   },
   ClaimCard: {
@@ -51,40 +52,63 @@ const Claim = (props) => {
     "accident2.jpeg",
   ];
 
+  const claimeeCardSrc = "./claimeeCardSrc.jpg";
+
   return (
     <div id="claim">
       <TopBar />
-      <ClientCard
-        personType="CLAIMANT"
-        name="Bailey Tincher"
-        policyNumber="ZZZ 9999-X15-88Z"
-        effective="Dec 11, 2014 - Dec 01, 2019"
-        make="FORD"
-        model="FUSION"
-        year="2015"
-        vin="Z99ZZ99Z123456780"
-        agent="JAKE"
-        agentNumber="(800) 782-8332"
-        coverage="COMPREHENSIVE"
-      />
+      <GridList className={classes.claimCards} style={{width:"100%", height:"auto"}}>
+        <GridListTile className={classes.claimCard} style={{width:"100%", height:"auto"}}>
+          <ClientCard
+            personType="CLAIMANT"
+            name="Bailey Tincher"
+            policyNumber="ZZZ 9999-X15-88Z"
+            effective="Dec 11, 2014 - Dec 01, 2019"
+            make="FORD"
+            model="FUSION"
+            year="2015"
+            vin="Z99ZZ99Z123456780"
+            agent="JAKE"
+            agentNumber="(800) 782-8332"
+            coverage="COMPREHENSIVE"
 
-      <ClaimeeCard
-        personType="CLAIMEE"
-        name="JACKIE OH"
-        policyNumber="ZZZ 8888-13-99X"
-        insurer="STATE FARM"
-        make="HONDA"
-        model="CIVIC"
-        year="2018"
-        vin="Z99ZZ99Z087654321"
-        effective="Apr 2, 2015 - Apr 1, 2020"
-        coverage="BODILY INJURY"
+            style={{width:"100%"}}
+          />
+        </GridListTile>
 
-        style={{width:"100%"}}
-      />
+        <GridListTile className={classes.claimCard} style={{width:"100%", height:"auto"}}>
+          <Card style={{height: "225px", paddingTop:"15px"}}>
+            <img src={claimeeCardSrc} style={{width:"100%", height: "auto", paddingTop:0}}/>
+          </Card>
+        </GridListTile>
+      </GridList>
 
+      <GridList className={classes.claimCards} style={{width:"100%", height:"auto"}}>
+        <GridListTile className={classes.claimCard} style={{width:"100%", height:"auto"}}>
+          <ClaimeeCard
+            personType="CLAIMEE"
+            name="JACKIE OH"
+            policyNumber="ZZZ 8888-13-99X"
+            insurer="STATE FARM"
+            make="HONDA"
+            model="CIVIC"
+            year="2018"
+            vin="Z99ZZ99Z087654321"
+            effective="Apr 2, 2015 - Apr 1, 2020"
+            coverage="BODILY INJURY"
 
-    <GridList style={{height:"120px"}} className={classes.AccidentImages}>
+            style={{width:"100%"}}
+          />
+        </GridListTile>
+
+        <GridListTile className={classes.claimCard} style={{width:"100%", height:"auto"}}>
+          <Card style={{height: "225px", paddingTop:"15px"}}>
+            <img src={claimeeCardSrc} style={{width:"100%", height: "auto", paddingTop:0}}/>
+          </Card>
+        </GridListTile>
+      </GridList>
+
+      <GridList style={{height:"120px"}} className={classes.AccidentImages}>
         {accidentImages.map(aImg => (
           <GridListTile className={classes.AccidentImage}>
             <Card style={{height:"100px"}}>
