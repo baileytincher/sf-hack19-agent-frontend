0129339f66e5ceb2235b699490b89a9f00fd3631
Bailey Tincher <btincher99@gmail.com>
2019-06-13 03:07:22 -0500

    Add accident images

--- src/Components/Claim/Claim.jsx
+++ src/Components/Claim/Claim.jsx
@@ -4,16 +4,44 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 import TopBar from './TopBar';
 import ClientCard from './ClientCard';
 import ClaimeeCard from './ClaimeeCard';
 
 const useStyles = makeStyles(theme => ({
+  AccidentImages: {
+    paddingLeft: "10px",
+    flexWrap: "nowrap",
+  },
+  AccidentImage: {
+    marginRight: "10px",
+    width: "100%"
+  },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+  },
 }));
 
 const Claim = (props) => {
   const classes = useStyles();
+
+  // const accidentImages = [
+  //   "accident1_1.jpg",
+  //   "accident1_2.jpg",
+  //   "accident1_3.jpg",
+  //   "accident1_4.jpg",
+  // ];
+  const accidentImages = [
+    "accident.jpg",
+    "accident.jpg",
+    "accident2.jpeg",
+  ];
 
   return (
     <div id="claim">
@@ -31,7 +59,7 @@ const Claim = (props) => {
         agentNumber="(800) 782-8332"
         coverage="COMPREHENSIVE"
       />
-    <ClaimeeCard
+      <ClaimeeCard
         personType="CLAIMEE"
         name="JACKIE OH"
         policyNumber="ZZZ 8888-13-99X"
@@ -43,7 +71,18 @@ const Claim = (props) => {
         effective="Apr 2, 2015 - Apr 1, 2020"
         coverage="BODILY INJURY"
       />
+      <GridList className={classes.AccidentImages}>
+        {accidentImages.map(aImg => (
+          <GridListTile className={classes.AccidentImage}>
+            <Card style={{height:"100px"}}>
+              <img src={aImg} style={{width:"100%"}} />
+            </Card>
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
+
+
   );
 };
 
