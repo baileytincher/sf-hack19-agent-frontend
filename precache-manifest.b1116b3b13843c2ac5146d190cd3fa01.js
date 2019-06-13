d10e9090910d660fce201b4b7ca71a53f7d26695
Bailey Tincher <btincher99@gmail.com>
2019-06-13 12:04:19 -0500

    Updates

---
+++ precache-manifest.b1116b3b13843c2ac5146d190cd3fa01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c506d72c7d0ee61efd10fcfcdf6ea19a",
+    "url": "btin.io/sf-hack19-agent-frontend/static/media/sf-logo-white.c506d72c.png"
+  },
+  {
+    "revision": "7f5b9af3802e2270bd96574b91cdfe17",
+    "url": "btin.io/sf-hack19-agent-frontend/static/media/sf-full-white.7f5b9af3.png"
+  },
+  {
+    "revision": "dc4b2fceb95fc6fecfed",
+    "url": "btin.io/sf-hack19-agent-frontend/static/js/runtime~main.986637ca.js"
+  },
+  {
+    "revision": "eb2ce0767e07909675b2",
+    "url": "btin.io/sf-hack19-agent-frontend/static/js/main.11958816.chunk.js"
+  },
+  {
+    "revision": "234fcda72a5a0d0bc68b",
+    "url": "btin.io/sf-hack19-agent-frontend/static/js/2.07d92253.chunk.js"
+  },
+  {
+    "revision": "fc7f6e34045343c4bafc31372253a402",
+    "url": "btin.io/sf-hack19-agent-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22909ba7049970343e128db128c7fbf4.js"
+  "btin.io/sf-hack19-agent-frontend/precache-manifest.b1116b3b13843c2ac5146d190cd3fa01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("btin.io/sf-hack19-agent-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
